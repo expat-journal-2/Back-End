79361a31d3f90ec0df155abacca4fdf02c2252a4
Jonathan Hanson <jhanson8@uoregon.edu>
2020-03-03 16:22:03 -0800

    updates

--- data/migrations/20200301071713_initial.js
+++ data/migrations/20200301071713_initial.js
@@ -1,42 +1,41 @@
-
 exports.up = async function(knex) {
-  await knex.schema.createTable('users', tbl =>{
-      tbl.increments()
-      tbl.string('email', 128)
+  await knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .string("email", 128)
       .notNullable()
       .unique()
-      .index()
-      tbl.string('password')
+      .index();
+    tbl.string("password").notNullable();
+    tbl
+      .string("username", 128)
       .notNullable()
-      tbl.string('username', 128)
+      .index();
+    tbl.string("image_URL");
+    tbl.string("bio", 256);
+  });
+  await knex.schema.createTable("stories", tbl => {
+    tbl.increments();
+    tbl
+      .string("name", 128)
       .notNullable()
-      .index()
-      tbl.string('image_URL')
-      tbl.string('bio', 256)
-  })
-  await knex.schema.createTable('stories', tbl =>{
-      tbl.increments()
-      tbl.string('name', 128)
-      .notNullable()
-      .index()
-      tbl.string('image_URL')
-      tbl.string('location', 128)
-      .index()
-      tbl.string('content', 800)
-      .notNullable()
-      tbl.string('author', 128)
-      .index()
-      tbl.string('user_id')
+      .index();
+    tbl.string("image_URL");
+    tbl.string("location", 128).index();
+    tbl.string("content", 800).notNullable();
+    tbl.string("author", 128).index();
+    tbl
+      .integer("user_id")
       .unsigned()
       .notNullable()
-      .references('id')
-      .inTable('users')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE')
-  })
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists('users')
-  await knex.schema.dropTableIfExists('stories')
+  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists("stories");
 };
